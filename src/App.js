29141f9737d2b39fa0875aa6ec5a238b7e5907fa
Adarsha A <adarshaadi1997@gmail.com>
2022-11-09 14:29:03 +0530

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction:column;
+  align-items:center;
+  margin:30px 0 10px;
+  font-family:Montserrat;
+`;
+const Header = styled.span`
+  color: black;
+  font-size:25px;
+  font-weight:bold;
+`;
+
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Container>
+      <Header>Expense Tracker</Header>
+    </Container>
   );
 }
 
